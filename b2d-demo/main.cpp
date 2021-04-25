ea3d21f589c42f11c394ae7656080b4a06fea4ff
Konstantin <memasdealer@gmail.com>
2021-04-25 21:15:03 +0600

    b2d create ground + rectangle

--- b2d-demo/main.cpp
+++ b2d-demo/main.cpp
@@ -12,8 +12,53 @@ sf::RenderWindow *window;
 tmx::MapLoader *mapLoader;
 
 int main() {
-	/*b2Vec2 grav = b2Vec2(0, 9.8f);
-	b2World world(grav);*/
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, 10.0f);
+
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(199.0f, 200.0f);
+
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(200.0f, 400.0f);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+	body->CreateFixture(&fixtureDef);
+
+	float timeStep = 1.0f / 60.0f;
+
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	/*for (int32 i = 0; i < 60; ++i)
+	{
+		world.Step(timeStep, velocityIterations, positionIterations);
+		b2Vec2 position = body->GetPosition();
+		float angle = body->GetAngle();
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	}*/
+
+	sf::RectangleShape rectangle(sf::Vector2f(70.f, 100.f));
+	rectangle.setFillColor(sf::Color(200, 180, 240));
+
+	sf::RectangleShape rectangle2(sf::Vector2f(199.0f, 200.0f));
+	rectangle2.setFillColor(sf::Color(0, 0, 240));
+	rectangle2.setPosition(sf::Vector2f(0.0f, 10.0f));
 
 	tmx::setLogLevel(tmx::Logger::Info | tmx::Logger::Error);
 
@@ -37,8 +82,17 @@ int main() {
 			}
 		}
 
+		world.Step(timeStep, velocityIterations, positionIterations);
+		b2Vec2 position = body->GetPosition();
+		float angle = body->GetAngle();
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+		rectangle.setPosition(sf::Vector2f(position.x, position.y));
+		rectangle.setRotation(angle);
+
 		window->clear();
-		window->draw(*mapLoader);
+		//window->draw(*mapLoader);
+		window->draw(rectangle2);
+		window->draw(rectangle);
 		window->display();
 	}
 	
