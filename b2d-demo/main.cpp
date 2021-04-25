78521ce412a1a93d030bafa90468009e3b377b3a
OverCart345 <82957545+OverCart345@users.noreply.github.com>
2021-04-26 00:02:18 +0600

    шар падает вверх

--- b2d-demo/main.cpp
+++ b2d-demo/main.cpp
@@ -16,38 +16,38 @@ float y = 0.0f;
 
 int main() {
 
-	//////////////////////////////////////
+	///////////////////////////////////////////////////////
 	//      гравитация + платформа
-	b2Vec2 gravity(0.0f, -0.2f);  //гравитация
+	b2Vec2 gravity(0.0f, -0.3f);  //гравитация
 	b2World world(gravity);
 
 	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(x, 0.0f);   // корды платформы
+	groundBodyDef.position.Set(x, 250.0f);   // корды платформы
 	b2CircleShape groundBox;
-	groundBox.m_p.Set(200.0f, 200.0f);
+	groundBox.m_p.Set(x, 0.0f);
 	groundBox.m_radius = 50.0f;
 
 	b2Body* groundBody = world.CreateBody(&groundBodyDef);
 	groundBody->CreateFixture(&groundBox, 0.0f);
 	  
-	///////////////////////////////////////
-	//         шар
+	//////////////////////////////////////////////////////
+	//                      шар
 
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(98.0f, 400.0f);
+	bodyDef.position.Set(50.0f, 600.0f);
 	b2Body* body = world.CreateBody(&bodyDef);
 
 	b2CircleShape dynamicBox;
-	dynamicBox.m_p.Set(200.0f, 200.0f);
+	dynamicBox.m_p.Set(0.0f, 0.0f);
 	dynamicBox.m_radius = 50.0f;
 
-	///////////////////////////////////////
+	///////////////////////////////////////////////////////
 	//         плотность, трение и кадры в секунду
 
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &dynamicBox;
-	fixtureDef.density = 10.0f;
+	fixtureDef.density = 1.0f;
 	fixtureDef.friction = 0.3f;
 
 	body->CreateFixture(&fixtureDef);
@@ -58,21 +58,26 @@ int main() {
 	int32 positionIterations = 2;
 
 	
-	////////////////////////////////////////////////
+	/////////////////////////////////////////////////////
 	//               создание фигур
 
-	sf::CircleShape rectangle;
-	rectangle.setFillColor(sf::Color(200, 180, 240));
-	rectangle.setRadius(50.0f);
 
+
+	sf::Texture Water;
+	Water.loadFromFile("water.jpg");
+
+	sf::CircleShape rectangle;
+	//rectangle.setFillColor(sf::Color(200, 180, 240));
+	rectangle.setRadius(50.0f);
+	rectangle.setTexture(&Water);
 
 	sf::CircleShape rectangle2;
 	rectangle2.setFillColor(sf::Color(0, 0, 240));
 	rectangle2.setRadius(50.0f);
-	rectangle2.setPosition(sf::Vector2f(x, 0.0f));
+	rectangle2.setPosition(sf::Vector2f(x, 250.0f));
 
-	//////////////////////////////////////////////////
-	//              открытие окна     
+	////////////////////////////////////////////////////
+	//              загрузка карты    
 
 
 	tmx::setLogLevel(tmx::Logger::Info | tmx::Logger::Error);
@@ -87,6 +92,9 @@ int main() {
 		return 1;
 	}
 
+	//////////////////////////////////////////////////////
+	//                  Открытие окна
+
 	window = new sf::RenderWindow(sf::VideoMode(1024, 768), "TEST");
 	while (window->isOpen()) {
 		sf::Event e;
@@ -97,12 +105,26 @@ int main() {
 			}
 		}
 
+		////////////////////////////////////////////////////////
+		//     получение позиции и угла подвижного шара
+
 		world.Step(timeStep, velocityIterations, positionIterations);
 		b2Vec2 position = body->GetPosition();
 		float angle = body->GetAngle();
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
 		rectangle.setPosition(sf::Vector2f(position.x, position.y));
 		rectangle.setRotation(angle);
+
+			
+		/*if (Keyboard::isKeyPressed(Keyboard::Up)) {
+		
+		
+		}*/
+
+
+		/////////////////////////////////////////////////////////////
+		//                    отрисовка
+
 
 		window->clear();
 		//window->draw(*mapLoader);
