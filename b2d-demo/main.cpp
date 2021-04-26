f055efa59fb5005642dee3e3a27117157b4d8216
OverCart345 <82957545+OverCart345@users.noreply.github.com>
2021-04-26 22:24:02 +0600

    2 шара 2 куба

--- b2d-demo/main.cpp
+++ b2d-demo/main.cpp
@@ -12,20 +12,19 @@
 sf::RenderWindow *window;
 tmx::MapLoader *mapLoader;
 
-float x = 0.0f;
-float y = 0.0f;
+
 
 int main() {
 
 	///////////////////////////////////////////////////////
 	//      гравитация + статический шар
-	b2Vec2 gravity(0.0f, -10.0f);  //гравитация
+	b2Vec2 gravity(0.0f, -1.0f);  //гравитация
 	b2World world(gravity);
 
 	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(x, 250.0f);   // корды шара
+	groundBodyDef.position.Set(0.0f, 250.0f);   // корды шара
 	b2CircleShape groundBox;
-	groundBox.m_p.Set(x, 0.0f);
+	groundBox.m_p.Set(0.0f, 0.0f);
 
 	groundBox.m_radius = 50.0f;
 
@@ -50,14 +49,71 @@ int main() {
 
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(50.0f, 600.0f);
+	bodyDef.position.Set(20.0f, 700.0f);
 	
 	b2Body* body = world.CreateBody(&bodyDef);
 
 	b2CircleShape dynamicBox;
 	dynamicBox.m_p.Set(0.0f, 0.0f);
+	
 	dynamicBox.m_radius = 25.0f;
+	
 
+	/////////////////////////////////////////////////////////
+	//                  коробка
+
+
+	b2BodyDef thebox;
+	thebox.type = b2_dynamicBody;
+	thebox.position.Set(600.0f, 500.0f);
+	
+	b2PolygonShape thedinamicbox;
+	thedinamicbox.SetAsBox(100.0f, 100.0f);
+	
+	b2Body* bodybox = world.CreateBody(&thebox);
+	bodybox->CreateFixture(&thedinamicbox, 0.0f);
+
+	b2FixtureDef fixtureDe;
+	fixtureDe.shape = &thedinamicbox;
+	fixtureDe.density = 1.0f;
+	fixtureDe.friction = 0.3f;
+
+	bodybox->CreateFixture(&fixtureDe);
+
+
+
+	/////////////////////////////////////////////////////////
+	//                    коробка 2
+
+
+	b2BodyDef thebox2;
+	thebox2.type = b2_dynamicBody;
+	thebox2.position.Set(400.0f, 300.0f);
+
+	b2PolygonShape thedinamicbox2;
+	thedinamicbox2.SetAsBox(50.0f, 50.0f);
+
+	b2Body* bodybox2 = world.CreateBody(&thebox2);
+	bodybox2->CreateFixture(&thedinamicbox2, 0.0f);
+
+	b2FixtureDef fixtureDe2;
+	fixtureDe2.shape = &thedinamicbox2;
+	fixtureDe2.density = 1.0f;
+	fixtureDe2.friction = 0.3f;
+
+	bodybox2->CreateFixture(&fixtureDe2);
+	/*b2Vec2 vertices[4];
+	vertices.type = b2_dynamicBody;
+	vertices[0].Set(600.0f, 300.0f);
+	vertices[1].Set(650.0f, 300.0f);
+	vertices[2].Set(600.0f, 400.0f);
+	vertices[3].Set(650.0f, 400.0f);
+
+	b2PolygonShape polygon;
+	polygon.Set(vertices, 4);
+	
+	b2Body* groundB = world.CreateBody(polygon);
+	groundB->CreateFixture(&bed, 0.0f);*/
 	///////////////////////////////////////////////////////
 	//         плотность, трение и кадры в секунду
 
@@ -92,12 +148,22 @@ int main() {
 	rectangle2.setFillColor(sf::Color(0, 0, 240));
 	rectangle2.setRadius(50.0f);
 	rectangle2.setOrigin(50.0f, 50.0f);
-	rectangle2.setPosition(sf::Vector2f(x, WINDOW_HEIGHT - 250.0f - 50));
+	rectangle2.setPosition(sf::Vector2f(0.0f, WINDOW_HEIGHT - 250.0f - 50));
 
 	sf::RectangleShape rectangle3(sf::Vector2f(1024.0f, 75.0f));
 	//rectangle.setFillColor(sf::Color(200, 180, 240));
 	rectangle3.setPosition(0.0f, WINDOW_HEIGHT - 75);
 	rectangle3.setTexture(&Water);
+
+	sf::RectangleShape rectangle4(sf::Vector2f(200.0f, 200.0f));
+	rectangle4.setFillColor(sf::Color(200, 180, 240));
+	rectangle4.setPosition(600.0f, WINDOW_HEIGHT - 500 - 150);
+	//rectangle3.setTexture(&Water);
+
+	sf::RectangleShape rectangle42(sf::Vector2f(100.0f, 100.0f));
+	rectangle42.setFillColor(sf::Color(100, 210, 140));
+	rectangle42.setPosition(490.0f, WINDOW_HEIGHT - 800);
+	//rectangle3.setTexture(&Water);
 
 	////////////////////////////////////////////////////
 	//              загрузка карты    
@@ -133,12 +199,21 @@ int main() {
 
 		world.Step(timeStep, velocityIterations, positionIterations);
 		b2Vec2 position = body->GetPosition();
+		b2Vec2 positions = bodybox->GetPosition();
+		b2Vec2 positions2 = bodybox2->GetPosition();
 		float angle = body->GetAngle();
+		float angle2 = bodybox->GetAngle();
+		float angle22 = bodybox2->GetAngle();
 		angle = -angle;
+		angle2 = -angle2;
+		angle22 = -angle22;
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
 		rectangle.setPosition(sf::Vector2f(position.x, WINDOW_HEIGHT - position.y - 50));
 		rectangle.setRotation(angle * DEG);
-
+		rectangle4.setPosition(sf::Vector2f(positions.x - 100, WINDOW_HEIGHT - positions.y - 150));
+		rectangle4.setRotation(angle2 * DEG);
+		rectangle42.setPosition(sf::Vector2f(positions2.x - 50, WINDOW_HEIGHT - positions2.y - 100));
+		rectangle42.setRotation(angle22 * DEG);
 			
 		/*if (Keyboard::isKeyPressed(Keyboard::Up)) {
 		
@@ -155,6 +230,8 @@ int main() {
 		window->draw(rectangle2);
 		window->draw(rectangle);
 		window->draw(rectangle3);
+		window->draw(rectangle4);
+		window->draw(rectangle42);
 		window->display();
 	}
 	
