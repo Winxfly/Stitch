d0c811fca429d15195f5962eee3b3472b67f0fc0
OverCart345 <82957545+OverCart345@users.noreply.github.com>
2021-04-26 17:33:58 +0600

    Шар падает вниз и катится

---
+++ b2d-demo/const.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const int WINDOW_WIDTH = 1024;
+const int WINDOW_HEIGHT = 768;
+
+const float DEG = 57.29577f;
+const float SCALE = 30.f;
--- b2d-demo/main.cpp
+++ b2d-demo/main.cpp
@@ -4,6 +4,7 @@
 
 #include "SFML/Graphics.hpp"
 
+#include "const.h"
 #include "tmx/MapLoader.hpp"
 #include "tmx/Log.hpp"
 #include "tmx/tmx2box2d.hpp"
@@ -17,18 +18,32 @@ float y = 0.0f;
 int main() {
 
 	///////////////////////////////////////////////////////
-	//      гравитаци€ + платформа
-	b2Vec2 gravity(0.0f, -0.3f);  //гравитаци€
+	//      гравитаци€ + статический шар
+	b2Vec2 gravity(0.0f, -10.0f);  //гравитаци€
 	b2World world(gravity);
 
 	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(x, 250.0f);   // корды платформы
+	groundBodyDef.position.Set(x, 250.0f);   // корды шара
 	b2CircleShape groundBox;
 	groundBox.m_p.Set(x, 0.0f);
+
 	groundBox.m_radius = 50.0f;
 
 	b2Body* groundBody = world.CreateBody(&groundBodyDef);
 	groundBody->CreateFixture(&groundBox, 0.0f);
+
+
+
+	///////////////////////////////////////////////////////
+	//                    платформа
+
+	b2BodyDef groundBod;
+	groundBod.position.Set(0.0f, 0.0f);
+	b2PolygonShape bed;
+	bed.SetAsBox(4000.0f, 25.0f);
+
+	b2Body* groundBo = world.CreateBody(&groundBod);
+	groundBo->CreateFixture(&bed, 0.0f);
 	  
 	//////////////////////////////////////////////////////
 	//                      шар
@@ -36,11 +51,12 @@ int main() {
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
 	bodyDef.position.Set(50.0f, 600.0f);
+	
 	b2Body* body = world.CreateBody(&bodyDef);
 
 	b2CircleShape dynamicBox;
 	dynamicBox.m_p.Set(0.0f, 0.0f);
-	dynamicBox.m_radius = 50.0f;
+	dynamicBox.m_radius = 25.0f;
 
 	///////////////////////////////////////////////////////
 	//         плотность, трение и кадры в секунду
@@ -68,13 +84,20 @@ int main() {
 
 	sf::CircleShape rectangle;
 	//rectangle.setFillColor(sf::Color(200, 180, 240));
-	rectangle.setRadius(50.0f);
+	rectangle.setRadius(25.0f);
+	rectangle.setOrigin(25.0f, 25.0f);
 	rectangle.setTexture(&Water);
 
 	sf::CircleShape rectangle2;
 	rectangle2.setFillColor(sf::Color(0, 0, 240));
 	rectangle2.setRadius(50.0f);
-	rectangle2.setPosition(sf::Vector2f(x, 250.0f));
+	rectangle2.setOrigin(50.0f, 50.0f);
+	rectangle2.setPosition(sf::Vector2f(x, WINDOW_HEIGHT - 250.0f - 50));
+
+	sf::RectangleShape rectangle3(sf::Vector2f(1024.0f, 75.0f));
+	//rectangle.setFillColor(sf::Color(200, 180, 240));
+	rectangle3.setPosition(0.0f, WINDOW_HEIGHT - 75);
+	rectangle3.setTexture(&Water);
 
 	////////////////////////////////////////////////////
 	//              загрузка карты    
@@ -95,7 +118,7 @@ int main() {
 	//////////////////////////////////////////////////////
 	//                  ќткрытие окна
 
-	window = new sf::RenderWindow(sf::VideoMode(1024, 768), "TEST");
+	window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "TEST");
 	while (window->isOpen()) {
 		sf::Event e;
 
@@ -111,9 +134,10 @@ int main() {
 		world.Step(timeStep, velocityIterations, positionIterations);
 		b2Vec2 position = body->GetPosition();
 		float angle = body->GetAngle();
+		angle = -angle;
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-		rectangle.setPosition(sf::Vector2f(position.x, position.y));
-		rectangle.setRotation(angle);
+		rectangle.setPosition(sf::Vector2f(position.x, WINDOW_HEIGHT - position.y - 50));
+		rectangle.setRotation(angle * DEG);
 
 			
 		/*if (Keyboard::isKeyPressed(Keyboard::Up)) {
@@ -130,6 +154,7 @@ int main() {
 		//window->draw(*mapLoader);
 		window->draw(rectangle2);
 		window->draw(rectangle);
+		window->draw(rectangle3);
 		window->display();
 	}
 	
