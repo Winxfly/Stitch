afe23ba297e3a05b9701280aaaef2b638d8581ae
Konstantin <memasdealer@gmail.com>
2021-04-27 14:46:50 +0600

    .0f

--- b2d-demo/main.cpp
+++ b2d-demo/main.cpp
@@ -14,7 +14,7 @@ tmx::MapLoader *mapLoader;
 const float DEG  =  57.29577f;
 
 float xGround = 0.0f;
-int angleGround = 15;
+float angleGround = 45.0f;
 
 int main() {
 	b2Vec2 gravity(0.0f, -9.81f);
@@ -23,17 +23,18 @@ int main() {
 	b2BodyDef groundBodyDef;
 	groundBodyDef.position.Set(xGround, 0.0f);
 
+
 	b2Body* groundBody = world.CreateBody(&groundBodyDef);
 	b2PolygonShape groundBox;
-	groundBox.SetAsBox(1000.0f, 50.0f, b2Vec2(0, 0), 0);
+	groundBox.SetAsBox(1000.0f, 50.0f, b2Vec2(500.0f, 25.0f), angleGround);
 	groundBody->CreateFixture(&groundBox, 1.0f);
-	groundBody->SetTransform(groundBody->GetPosition(), -angleGround);
+	//groundBody->SetTransform(groundBody->GetPosition(), -angleGround);
 
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(400.0f, 500.0f);
+	bodyDef.position.Set(600.0f, 500.0f);
 	b2Body* body = world.CreateBody(&bodyDef);
-	body->SetTransform(body->GetPosition(), 45);
+	body->SetTransform(body->GetPosition(), 45.0f);
 
 	b2PolygonShape dynamicBox;
 	dynamicBox.SetAsBox(35.0f, 50.0f);
@@ -53,12 +54,12 @@ int main() {
 	sf::RectangleShape rectangle(sf::Vector2f(70.f, 100.f));
 	rectangle.setFillColor(sf::Color(200, 180, 240));
 	rectangle.setOrigin(35.0f, 50.0f);
-	rectangle.setRotation(45);
+	rectangle.setRotation(45.0f);
 
 	sf::RectangleShape rectangle2(sf::Vector2f(1000.0f, 100.0f));
 	rectangle2.setFillColor(sf::Color(0, 0, 240));
-	//rectangle2.setOrigin(500.0f, 50.0f);
-	rectangle2.setPosition(sf::Vector2f(xGround, 768.0f - 100.0f));
+	rectangle2.setOrigin(500.0f, 50.0f);
+	rectangle2.setPosition(sf::Vector2f(xGround + 500.0f, 768.0f - 50.0f));
 	rectangle2.setRotation(angleGround  * (-1));
 
 	tmx::setLogLevel(tmx::Logger::Info | tmx::Logger::Error);
@@ -88,7 +89,7 @@ int main() {
 		//body->SetAngularVelocity(-10.0f);
 		float angle = body->GetAngle();
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle * DEG);
-		rectangle.setPosition(sf::Vector2f(position.x, 768.0f - position.y - 50.0f));
+		rectangle.setPosition(sf::Vector2f(position.x + 35.0f, 768.0f - position.y - 50.0f));
 		rectangle.setRotation(angle * DEG * (-1));
 
 		window->clear();
